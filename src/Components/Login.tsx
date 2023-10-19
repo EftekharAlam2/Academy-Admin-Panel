5c456dbf948600aee6fd3ff9bb1c8d1ec87f764e
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-10-19 06:22:19 +0600

    Login page edit

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Login = () => {
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignIn = () => {
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md w-96">
+        <h1 className="text-2xl font-semibold text-center mb-4">Sign In</h1>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Email
+          </label>
+          <input
+            type="email"
+            className="w-full px-3 py-2 border rounded shadow appearance-none"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Password
+          </label>
+          <input
+            type="password"
+            className="w-full px-3 py-2 border rounded shadow appearance-none"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button
+          className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+          onClick={handleSignIn}
+        >
+          Sign In
+        </button>
+      </div>
     </div>
   );
 };
 
-export default Login;
+export default SignIn;
